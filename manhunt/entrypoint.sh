a8a3060d127f8d03ddcf93705e9334ab6aaa42ab
Harry W <hjohnw13@gmail.com>
2021-10-26 22:45:47 +0100

    (manhunt) Update wget command

--- manhunt/entrypoint.sh
+++ manhunt/entrypoint.sh
@@ -3,7 +3,7 @@ cd /home/container || exit
 echo "Deleting all server files."
 rm -r /home/container/*
 echo "Downloading new server files from https://templates.exeomc.net/gamemode/manhunt"
-wget -O https://templates.exeomc.net/gamemode/manhunt
+wget –quiet https://templates.exeomc.net/gamemode/manhunt
 echo "Starting server."
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')

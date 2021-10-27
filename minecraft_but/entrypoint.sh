8ea480168da129d6ff27f400555c39a3f7ab96d1
KingWithOutPing <davebdave123@gmail.com>
2021-10-27 12:02:36 +0100

    Fix URLS (2)

--- minecraft_but/entrypoint.sh
+++ minecraft_but/entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+
 cd /home/container || exit
+
+echo "==============================================================="
 echo "Deleting all server files."
+echo "==============================================================="
+
 rm -r /home/container/*
+
+echo "==============================================================="
 echo "Downloading new server files from https://templates.exeomc.net/gamemode/minecraft_but"
-wget –quiet https://templates.exeomc.net/gamemode/manhunt
+echo "==============================================================="
+
+wget https://templates.exeomc.net/gamemode/manhunt
+
+echo "==============================================================="
 echo "Starting server."
+echo "==============================================================="
+
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 eval ${MODIFIED_STARTUP}

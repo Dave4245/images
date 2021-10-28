699dd630b4017d239651b3e2822156146940d611
KingWithOutPing <davebdave123@gmail.com>
2021-10-28 18:39:40 +0100

    Test 4

--- manhunt/entrypoint.sh
+++ manhunt/entrypoint.sh
@@ -2,21 +2,21 @@
 
 cd /home/container || exit
 
-echo "==============================================================="
-echo "Deleting all server files."
-echo "==============================================================="
+echo "==============================================================================="
+echo "-> Deleting all server files."
+echo "==============================================================================="
 
 rm -r /home/container/*
 
-echo "==============================================================="
-echo "Downloading new server files from https://templates.exeomc.net/manhunt/"
-echo "==============================================================="
+echo "==============================================================================="
+echo "-> Downloading new server files from https://templates.exeomc.net/manhunt/"
+echo "==============================================================================="
 
-wget -r -np -nd -R "index.php*" https://templates.exeomc.net/manhunt/
+wget -r -np -nd -R "index.php*" 'https://templates.exeomc.net/manhunt'
 
-echo "==============================================================="
-echo "Starting server."
-echo "==============================================================="
+echo "==============================================================================="
+echo "-> Starting server."
+echo "==============================================================================="
 
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')

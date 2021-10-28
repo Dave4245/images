f65fcd13db18aa2f5fb7bffb824be21a0d41b709
KingWithOutPing <davebdave123@gmail.com>
2021-10-28 15:37:57 +0100

    Test 1

--- manhunt/entrypoint.sh
+++ manhunt/entrypoint.sh
@@ -12,7 +12,7 @@ echo "==============================================================="
 echo "Downloading new server files from https://templates.exeomc.net/manhunt/"
 echo "==============================================================="
 
-wget -R html -r -np https://templates.exeomc.net/manhunt/
+wget -r -np -R "index.html*" https://templates.exeomc.net/manhunt/
 
 echo "==============================================================="
 echo "Starting server."

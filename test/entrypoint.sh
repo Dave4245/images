1f706c3acdc67432602d16617e25f4580534474b
KingWithOutPing <davebdave123@gmail.com>
2021-11-11 02:21:36 +0000

    Update

---
+++ test/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+java -version
+
+echo "==============================================================="
+echo "Downloading server files."
+echo "==============================================================="
+
+cp -r /data/ /home/container/
+
+cd /home/container || exit
+
+cat > ServerName.txt <<- "EOF"
+${SERVER_NAME}
+EOF
+
+echo "==============================================================="
+echo "Starting server."
+echo "==============================================================="
+
+eval "java -Xms128M -Xmx7000M --enable-preview -jar server.jar"

e47481966389d6d7e4f28356f65c4babdd775026
KingWithOutPing <davebdave123@gmail.com>
2021-11-12 00:08:09 +0000

    Update

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -8,14 +8,14 @@ echo "==============================================================="
 
 sleep 5
 
-ls /home/container
-rm -rv /home/container/
-cp -rv /data/. /home/container
+ls /home/container/data
+rm -rv /home/container/data/
+cp -rvT /data/ /home/container/data/
 hostname > /home/container/server-name.txt
 
 echo "==============================================================="
 echo "Starting server."
 echo "==============================================================="
 
-cd /home/container || exit
+cd /home/container/data || exit
 eval "java -Xms128M -Xmx5G --enable-preview -jar server.jar"

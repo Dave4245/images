a24b076214fb147a7e450ee51240412445e1874f
KingWithOutPing <davebdave123@gmail.com>
2021-11-16 18:57:10 +0000

    Update

--- bungee/entrypoint.sh
+++ bungee/entrypoint.sh
@@ -8,8 +8,7 @@ echo "==============================================================="
 
 
 rm -rv /home/container/data/
-cd /data || exit
-cp -Rv . /home/container/data/
+cp -RvT /data/ /home/container/data/
 hostname > /home/container/data/server-name.txt
 
 echo "==============================================================="
@@ -17,5 +16,4 @@ echo "Starting server."
 echo "==============================================================="
 
 cd /home/container/data || exit
-ls
 exec java -Xms128M -Xmx1G --enable-preview -jar server.jar

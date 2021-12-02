899cb1ec53c4b2324bf4646a5ecffaf8deb983a5
KingWithOutPing <davebdave123@gmail.com>
2021-12-02 17:08:48 +0000

    Update

---
+++ death_swap/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+java -version
+
+echo "==============================================================="
+echo "Downloading server files."
+echo "==============================================================="
+
+rm -rv /home/container/data/
+cp -RvT /data/ /home/container/data/
+hostname > /home/container/data/hostname.txt
+
+echo "==============================================================="
+echo "Starting server."
+echo "==============================================================="
+
+cd /home/container/data || exit
+exec java -Xms128M -Xmx4G --enable-preview -jar server.jar
+

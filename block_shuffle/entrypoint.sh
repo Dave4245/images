4dd4447d66565461e79291930da8206331a98ecc
KingWithOutPing <davebdave123@gmail.com>
2021-12-13 09:43:41 +0000

    Parent path fix final 4

--- block_shuffle/entrypoint.sh
+++ block_shuffle/entrypoint.sh
@@ -10,8 +10,8 @@ rm -rv /home/container/data/
 mkdir -pv /home/container/data/plugins/
 
 cp -rv /data/servers/plugins/BlockShuffle.jar /home/container/data/plugins/
-cp -rv /data/servers/configs/minigame /home/container/data/
-cp -rv /data/servers/plugins/core /home/container/data/plugins/
+cp -rv /data/servers/configs/minigame/* /home/container/data/
+cp -rv /data/servers/plugins/core/* /home/container/data/plugins/
 
 hostname > /home/container/data/hostname.txt
 

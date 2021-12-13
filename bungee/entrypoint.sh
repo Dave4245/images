1792b0499d8977284822f8cc149affb0d623982b
KingWithOutPing <davebdave123@gmail.com>
2021-12-13 09:02:04 +0000

    Parent path fix 5

--- bungee/entrypoint.sh
+++ bungee/entrypoint.sh
@@ -7,6 +7,7 @@ echo "Downloading server files."
 echo "==============================================================="
 
 rm -rv /home/container/data/
+mkdir -pv /home/container/data/plugins/
 
 cp -RpvT /data/servers/plugins/bungee/ /home/container/data/plugins/
 cp -RpvT /data/servers/configs/bungee/ /home/container/data/

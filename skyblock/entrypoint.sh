ce69ee015809d3328403948aca3c12c97da38970
KingWithOutPing <davebdave123@gmail.com>
2021-12-26 02:33:39 +0000

    Update for all

---
+++ skyblock/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+java -version
+
+echo "==============================================================="
+echo "Downloading server files."
+echo "==============================================================="
+
+hostname > /home/container/hostname.txt
+echo "false" > /home/container/development_server.txt
+
+echo "==============================================================="
+echo "Starting server."
+echo "==============================================================="
+
+cd /home/container/data || exit
+exec java -Xms128M -Xmx8G --enable-preview -jar server.jar
+

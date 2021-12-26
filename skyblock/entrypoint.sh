5ab5bdfd6ec64cf3dc1ed3e6ce25cdf3c16103da
KingWithOutPing <davebdave123@gmail.com>
2021-12-26 02:40:18 +0000

    Update for all

--- skyblock/entrypoint.sh
+++ skyblock/entrypoint.sh
@@ -6,8 +6,8 @@ echo "==============================================================="
 echo "Downloading server files."
 echo "==============================================================="
 
-hostname > /home/container/hostname.txt
-echo "false" > /home/container/development_server.txt
+hostname > /home/container/data/hostname.txt
+echo "false" > /home/container/data/development_server.txt
 
 echo "==============================================================="
 echo "Starting server."

a3e094f6aa603ea9183e23643b796640ee7c53dd
Dave4245 <davebdave123@gmail.com>
2022-01-14 19:09:18 +0000

    Minigame Update
    Took 3 minutes

---
+++ minigame/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+java -version
+
+LOCATION=""
+IS_DEVELOPMENT_SERVER=""
+
+if test -z "$DEVELOPMENT_SERVER"; then
+  LOCATION="production"
+  IS_DEVELOPMENT_SERVER="false"
+else
+  LOCATION="development"
+  IS_DEVELOPMENT_SERVER="true"
+fi
+
+echo "==============================================================="
+echo "Downloading server files."
+echo "==============================================================="
+
+rm -rv /home/data/
+mkdir -pv /home/data/plugins/
+
+cp -rv /data/$LOCATION/servers/plugins/Minigame.jar /home/data/plugins
+cp -rv /data/$LOCATION/servers/configs/minigame/* /home/data
+cp -rv /data/$LOCATION/servers/plugins/core/* /home/data/plugins
+
+hostname > /home/data/hostname.txt
+echo "${IS_DEVELOPMENT_SERVER}" > /home/data/development_server.txt
+
+echo "==============================================================="
+echo "Starting server. (This is a ${LOCATION} server)"
+echo "==============================================================="
+
+cd /home/data || exit
+exec java -Xms128M -Xmx6G -Dlog4j2.formatMsgNoLookups=true --enable-preview -jar server.jar
+
